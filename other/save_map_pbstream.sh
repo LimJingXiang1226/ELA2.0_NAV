d7b88ccd5339fbb67d3bfc11c958d12d853ea31c
LimJingXiang1226 <limjx1226@gmail.com>
2024-06-27 13:02:25 +0800

    Update save_map_pbstream.sh

--- other/save_map_pbstream.sh
+++ other/save_map_pbstream.sh
@@ -1,4 +1,12 @@
-cd
-ros2 run nav2_map_server map_saver_cli -f ~/ELA2.0_NAV/install/ella2_nav/share/ella2_nav/maps/apcore2
-ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: 'apcore2.pbstream'}" && mv ~/apcore2.pbstream ~/ELA2.0_NAV/install/ella2_nav/share/ella2_nav/maps
+#!/bin/bash
 
+# Check if FILENAME environment variable is set
+if [ -z "$MAP_FILENAME" ]; then
+    echo "Please set the MAP_FILENAME environment variable."
+    echo "Usage: export MAP_FILENAME=<filename> && ./save_map.sh"
+    exit 1
+fi
+
+cd
+ros2 run nav2_map_server map_saver_cli -f ~/ELA2.0_NAV/install/ella2_nav/share/ella2_nav/maps/${MAP_FILENAME}
+ros2 service call /write_state cartographer_ros_msgs/srv/WriteState "{filename: '${MAP_FILENAME}.pbstream'}" && cp ~/${MAP_FILENAME}.pbstream ~/ELA2.0_NAV/src/ella2_nav/maps && mv ~/${MAP_FILENAME}.pbstream ~/ELA2.0_NAV/install/ella2_nav/share/ella2_nav/maps
